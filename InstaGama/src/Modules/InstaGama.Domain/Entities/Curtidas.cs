c0d433ab683cc12fa2e91c1a9558c36bf7967aa0
Karol <karolroberta92@hotmail.com>
2021-02-13 17:22:41 -0300

    Criando a Classe Curtidas

---
+++ InstaGama/src/Modules/InstaGama.Domain/Entities/Curtidas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    public class Curtidas
+    {
+		public Curtidas(int postagemId,
+						int usuarioId)
+		{
+			PostagemId = postagemId;
+			UsuarioId = usuarioId;
+		}
+
+		public Curtidas(int id,
+						int postagemId,
+						int usuarioId)
+		{
+			Id = id;
+			PostagemId = postagemId;
+			UsuarioId = usuarioId;
+		}
+
+		public int Id { get; private set; }
+		public int PostagemId { get; private set; }
+		public int UsuarioId { get; private set; }
+	}
+
+
+}
