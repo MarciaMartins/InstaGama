2645146bf0ed6d791223d3b239eaedd4f90e3473
Wendy-Anna <wendyufpb@gmail.com>
2021-02-13 14:45:03 -0300

    Correcoes

---
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using InstaGama.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGama.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<int> InserirAsync(Usuario  usuario);
+    }
+}
---
+++ InstaGama/src/Modules/InstaGama.Repositories/UsuarioRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace InstaGama.Repositories
+{
+    public  UsuarioRepository : IUsuarioRepository
+    {
+
+    }
+}
