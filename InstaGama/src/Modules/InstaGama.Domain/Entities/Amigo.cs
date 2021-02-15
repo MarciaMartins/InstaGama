afcb49bbe86436fec5360a173eadfa4d9a621995
MarciaMartins <marciapmartins@gmail.com>
2021-02-15 11:53:35 +0000

    Criação da entidade Amigo

---
+++ InstaGama/src/Modules/InstaGama.Domain/Entities/Amigo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    public class Amigo
+    {
+        public Amigo(UsuarioId usuarioId, UsuarioAmigoId usuarioAmigoId){
+            UsuarioId=usuarioId;
+            UsuarioAmigoId=usuarioAmigoId;
+        }
+       
+        public int Id { get; private set; }
+	    public UsuarioId UsuarioId { get; private set; }
+	    public UsuarioAmigoId UsuarioAmigoId { get; private set; }
+    
+        
+        public void SetId(int id)
+        {
+            Id = id;
+        }    
+    }    
+}
