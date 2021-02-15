2a005dfe994b8cc4f7e18a7e179a79ac5871b9ac
MarciaMartins <marciapmartins@gmail.com>
2021-02-15 12:19:59 +0000

    Inclusão da interface referente a classe amigo

---
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/IAmigoRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using InstaGama.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGama.Domain.Interfaces
+{
+    public interface IAmigoRepository
+    {
+        Task<int> InsertAsync(Amigo amigo);
+        Task<List<Amigo>> GetByAmigoIdAsync(int amigoId);
+    }
+}
